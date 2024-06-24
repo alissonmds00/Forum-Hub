67ab0992c8d619d90f03f7b8a648d8ec3494f69a
Alisson Matias <alissonmds00@gmaill..com>
2024-06-24 20:04:39 -0300

    First Commit

---
+++ src/main/java/dev/alisson_matias/Forum/Hub/ForumHubApplication.java
@@ -0,0 +1,13 @@
+package dev.alisson_matias.Forum.Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alisson_matias/Forum/Hub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alisson_matias.Forum.Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
