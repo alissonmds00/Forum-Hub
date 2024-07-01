0cba71901573006f336365061ee6f8c05a4ca06a
Alisson Matias <alissonmds00@gmaill..com>
2024-07-01 00:09:41 -0300

    feat: adicionada a busca de usuários por ano e nome do curso + busca por id

--- src/main/java/dev/alisson_matias/Forum/Hub/controller/TopicosController.java
+++ src/main/java/dev/alisson_matias/Forum/Hub/controller/TopicosController.java
@@ -1,14 +1,17 @@
 package dev.alisson_matias.Forum.Hub.controller;
 
-import dev.alisson_matias.Forum.Hub.domain.topico.*;
+import dev.alisson_matias.Forum.Hub.domain.topico.DadosCadastramentoTopico;
+import dev.alisson_matias.Forum.Hub.domain.topico.DadosDetalhamentoTopico;
+import dev.alisson_matias.Forum.Hub.domain.topico.RegistroDeTopico;
+import dev.alisson_matias.Forum.Hub.domain.topico.TopicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -26,6 +29,26 @@ public class TopicosController {
         var topico = registroDeTopico.criarNovoTopico(dados);
         var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosDetalhamentoTopico(topico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoTopico>> listarTopicos(@PageableDefault(size = 10) Pageable page) {
+        var topicos = repository.listar10UltimosTopicos(page).map(DadosDetalhamentoTopico::new);
+        return ResponseEntity.ok(topicos);
+    }
+
+    @GetMapping("/busca")
+    public ResponseEntity<Page<DadosDetalhamentoTopico>> listarTopicosPorCursoEAno(@PageableDefault(size = 10)String curso, int ano, Pageable page) {
+        var nomeCursoFormatado = curso.replaceAll("\\+", " ");
+        var anoFormatado = Long.valueOf(ano);
+        var cursos = repository.listarTopicosPorCursoEAnoDeCriacao(nomeCursoFormatado, anoFormatado, page).map(DadosDetalhamentoTopico::new);
+        return ResponseEntity.ok(cursos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity buscarTopicoPorId(@PathVariable Long id) {
+        var topico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoTopico(topico));
     }
 
 }
--- src/main/java/dev/alisson_matias/Forum/Hub/domain/topico/TopicoRepository.java
+++ src/main/java/dev/alisson_matias/Forum/Hub/domain/topico/TopicoRepository.java
@@ -1,6 +1,15 @@
 package dev.alisson_matias.Forum.Hub.domain.topico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    @Query("SELECT t FROM Topico t ORDER BY t.dataDeCriacao ASC")
+    Page<Topico> listar10UltimosTopicos(Pageable pagina);
+
+    //@Query("SELECT t FROM Topico t WHERE t.curso.nome ILIKE :nomeCursoFormatado AND YEAR(t.dataDeCriacao) = :anoFormatado")
+    @Query("SELECT t FROM Topico t WHERE LOWER(t.curso.nome) LIKE LOWER(CONCAT('%', :nomeCursoFormatado, '%')) AND YEAR(t.dataDeCriacao) = :anoFormatado")
+    Page<Topico> listarTopicosPorCursoEAnoDeCriacao(String nomeCursoFormatado, Long anoFormatado, Pageable pagina);
 }
