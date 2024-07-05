38c1e718a6f3cff93284ae97c44d0c1a0addf989
Alisson Matias <alissonmds00@gmaill..com>
2024-07-05 01:30:45 -0300

    feat: hotfix

--- src/main/java/dev/alisson_matias/Forum/Hub/controller/CursosController.java
+++ src/main/java/dev/alisson_matias/Forum/Hub/controller/CursosController.java
@@ -6,14 +6,11 @@ import dev.alisson_matias.Forum.Hub.domain.curso.DadosDetalhamentoCurso;
 import dev.alisson_matias.Forum.Hub.domain.curso.RegistrarCurso;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cursos")
@@ -36,7 +33,7 @@ public class CursosController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<DadosDetalhamentoCurso>> listarPacientesAtivos(@PageableDefault(size = 10, sort = {"nome"}) Pageable pagina)  {
+    public ResponseEntity<Page<DadosDetalhamentoCurso>> listarCurso(@PageableDefault(size = 10, sort = {"nome"}) Pageable pagina)  {
         var cursos = repository.findAll(pagina).map(DadosDetalhamentoCurso::new);
         return ResponseEntity.ok(cursos);
     }
